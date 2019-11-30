9e4e6a63ff5661ab50acc0940d12cbf037bc77ca
Wataru-Miyamoto <ponmiyapon43@gmail.com>
2019-11-30 12:59:51 +0000

    Finish top-page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,23 @@
 Rails.application.routes.draw do
-  get 'static_pages/top'
+  get 'sessions/new'
+
+  root 'static_pages#top'
+  get '/signup', to: 'users#new'
+  
+  get     '/login', to: 'sessions#new'
+  post    '/login', to: 'sessions#create'
+  delete  '/logout', to: 'sessions#destroy'
+  
+  
+  resources :users do
+    member do
+      get 'edit_basic_info'
+      patch 'update_basic_info'
+      get 'attendances/edit_one_month'
+      patch 'attendances/update_one_month'
+    end
+    resources :attendances, only: :update
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

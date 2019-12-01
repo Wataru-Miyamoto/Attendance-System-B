cd90dd7408fa675e233e590c08752d325598beac
Wataru-Miyamoto <ponmiyapon43@gmail.com>
2019-12-01 11:51:52 +0000

    layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,13 +14,13 @@ class UsersController < ApplicationController
   end
 
   def new
-    @user = User.new # ユーザーオブジェクトを生成し、インスタンス変数に代入します。
+    @user = User.new
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user # 保存成功後、ログインします。
+      log_in @user
       flash[:success] = '新規作成に成功しました。'
       redirect_to @user
     else
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
   root 'static_pages#top'
   get '/signup', to: 'users#new'
   
@@ -18,6 +16,4 @@ Rails.application.routes.draw do
     end
     resources :attendances, only: :update
   end
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

61ff049a5ff77c966ab42b136a943a2740c767cc
Wataru-Miyamoto <ponmiyapon43@gmail.com>
2019-12-05 10:09:29 +0000

    japanization-and-time-format

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+#Pagination label.
+WillPaginate::ViewHelpers.pagination_options[:previous_label] = '<<'
+WillPaginate::ViewHelpers.pagination_options[:next_label] = '>>'
